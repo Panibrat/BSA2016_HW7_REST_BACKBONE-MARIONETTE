00289f7a30e48430dbbe6e5730ebdf5fbce38e07
Panibrat <a.panibratenko@gmail.com>
2016-07-30 01:29:04 +0300

    Not all ready. Added All Books View + Single Book View

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -48,7 +48,7 @@
             <td><%= genre %></td>
             <td><%= year %></td>
             <td><%= user_id %></td>
-            <td><a href="#show/<%= id %>" class="btn btn-info" role="button">Show</a></td>
+            <td><a href="#books/<%= id %>" class="btn btn-info" role="button">Show</a></td>
             <td><a href="#books/delete/<%= id %>" class="btn btn-danger" role="button">Delete</a></td>
 
         </script>
@@ -138,6 +138,17 @@
                     });
                     allBooksView.render();
                 },
+                SingleBook: function(id){
+                    console.log(id + ' SingleBookPage???');
+                    var book = new App.Models.Book({id: id});
+                    book.fetch();
+                    viewBook = new  App.Views.Book({
+                        model: book,
+                        el: '.table tbody'
+                    });
+                    viewBook.render();
+
+                },
                 destroy: function(id){
                     console.log(id);
                     var book = new App.Models.Book({id: id});
@@ -145,6 +156,9 @@
                     Backbone.history.navigate('/books', {
                         trigger: true
                      });
+                },
+                usersBooks: function(userId){
+                    console.log(userId +  'usersBooksController');
                 }
             });
             myRouter = Marionette.AppRouter.extend({
@@ -152,7 +166,9 @@
                 appRoutes: {
                     "": "index",
                     "books": "books",
-                    "books/delete/:id": "destroy"
+                    "books/:id": "SingleBook",
+                    "books/delete/:id": "destroy",
+                    'users/:userId/books': 'usersBooks'
                 }
             });
 
@@ -181,27 +197,36 @@
         Backbone.history.start();
 
 
+//            validate: function (attrs) {
+//                console.log(attrs);
+//
+//                if (!attrs.firstname) {
+//                    return 'Обязательно';
+//                }
+//                if (!attrs.lastname) {
+//                    return 'Обязательно';
+//                }
+//                if (!attrs.email) {
+//                    return 'Обязательно';
+//                }
 
 
 
 
-
-//            RecordModel = Backbone.Model.extend({
-//                urlRoot: 'http://localhost:8000/api/userbook'
-//            });
 //
 //            RegisterList = Backbone.Collection.extend({
 //                url: 'http://localhost:8000/api/userbook',
 //                model: RecordModel
 //            });
 
-
-
-//            var book = new BookModel({id: id});
-//            book.destroy();
-//            Backbone.history.navigate('/books', {
-//                trigger: true
+//            books.fetch({
+//                success: function (coll) {
+//                    MyApp.content.show(new BooksView({
+//                        collection: coll
+//                    }));
+//                }
 //            });
+
 
         </script>
 
